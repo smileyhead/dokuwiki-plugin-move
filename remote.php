a99025d4b97e1fc765ba278bb3e080e83bc33ed0
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-05-24 19:57:50 +0200

    Add a plugin.move.rename XMLRPC method.
    The code is extremely simple. It reuses the renameOkay() function from
    action/rename.php

---
+++ remote.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * DokuWiki Plugin move (Helper Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author Claus-Justus Heine <himself@claus-justus-heine.de>
+ */
+
+class remote_plugin_move extends DokuWiki_Remote_Plugin
+{
+    /**
+     * Rename function.
+     *
+     * @param string $fromId
+     * @param string $toId
+     * @return int
+     */
+    public function rename(string $fromId, string $toId)
+    {
+        /** @var helper_plugin_move_op $MoveOperator */
+        $moveOperator = plugin_load('helper', 'move_op');
+        $moveAction = plugin_load('action', 'move_rename');
+
+        if (!$moveAction->renameOkay($fromId)) {
+            return 0;
+        }
+
+        if (!$moveOperator->movePage($fromId, $toId)) {
+            return 0;
+        }
+
+        return 1;
+    }
+}
