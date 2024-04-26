desolat/DokuWiki-Pagemove-Plugin

commit 0af31bb63abd3731e904efc2ed90dd3ce33f4e52

Media manager: early check if user is allowed to rename files
Checks if user matches the 'allowrename' setting

action/rename.php
@@ -35,7 +35,11 @@ class action_plugin_move_rename extends DokuWiki_Action_Plugin {
     public function handle_init() {
         global $JSINFO;
         global $INFO;
+        global $INPUT;
+        global $USERINFO;
+
         $JSINFO['move_renameokay'] = $this->renameOkay($INFO['id']);
+        $JSINFO['move_allowrename'] = auth_isMember($this->getConf('allowrename'), $INPUT->server->str('REMOTE_USER'), (array) $USERINFO['grps']);
     }
 
     /**

script/MoveMediaManager.js
@@ -5,20 +5,23 @@
 class MoveMediaManager {
 
     constructor() {
+        // user is not allowed to move anything
+        if (!JSINFO.move_allowrename) return;
+
         const filePanel = document.querySelector('#mediamanager__page .panel.file');
         if (filePanel) {
-            const observer = new MutationObserver(this.#addEditButton.bind(this));
+            const observer = new MutationObserver(this.#addMoveButton.bind(this));
             observer.observe(filePanel, {childList: true, subtree: true});
         }
     }
 
     /**
-     * Observer callback to add the edit button in the detail panel of the media manager
+     * Observer callback to add the move button in the detail panel of the media manager
      *
      * @param mutationsList
      * @param observer
      */
-    async #addEditButton(mutationsList, observer) {
+    async #addMoveButton(mutationsList, observer) {
         for (let mutation of mutationsList) {
             // div.file has been filled with new content?
             if (mutation.type !== 'childList') continue;

smileyhead/dokuwiki-plugin-move

commit f175a7aa86dc33121afa73b5abc9bb516d367136

Fix PHP warning when user not logged in
Warning: Trying to access array offset on value of type null in
./dokuwiki/move/action/rename.php on line 42

$USERINFO is null when user is not logged in.

Regression introduced by 0af31bb63abd3731e904efc2ed90dd3ce33f4e52 (#246).

Fixes #256, #255

action/rename.php
@@ -39,7 +39,11 @@ class action_plugin_move_rename extends DokuWiki_Action_Plugin {
         global $USERINFO;
 
         $JSINFO['move_renameokay'] = $this->renameOkay($INFO['id']);
-        $JSINFO['move_allowrename'] = auth_isMember($this->getConf('allowrename'), $INPUT->server->str('REMOTE_USER'), (array) $USERINFO['grps']);
+        $JSINFO['move_allowrename'] = auth_isMember(
+            $this->getConf('allowrename'),
+            $INPUT->server->str('REMOTE_USER'),
+            $USERINFO['grps'] ?? []
+        );
     }
 
     /**